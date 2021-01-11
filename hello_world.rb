285804dd95c595e607e40749abdb4587b69019e2
Kwaku Gyapong <kwakugyapong219@gmail.com>
2021-01-11 14:52:06 +0000

    Automatically backed up by Learn

---
+++ hello_world.rb
@@ -0,0 +1,5 @@
+def greeting_programmer(name, language, car)
+  puts "Hello, #{name}. We heard you are a great #{language} programmer. I drive an #{car}"
+end
+ 
+greeting_programmer("Maria", "Ruby" , "BMW")
